56b124c50bdff76423e718e115b82076fa8e9653
Camilo <camilo@e-advising.biz>
2018-11-28 22:07:17 -0500

    Javascript
    * Adicion de operaciones

--- 4javaScript_CamiloArturo_Perez/js/app.js
+++ 4javaScript_CamiloArturo_Perez/js/app.js
@@ -9,31 +9,52 @@ class Calculadora {
 
         */
 
+        this.adicionarPropiedades( )  ;
         this.eventoClic( )  ;
+
+
 
 
     }
 
+    adicionarPropiedades(){
 
-    eventoClic  (  )   {
+        var tipo = true ? "hidden" : "text";
+
+        var input = document.createElement("input");
+        input.type = tipo;
+        input.name = "operSeleccionado";
+        input.id = "operSeleccionado";
+        document.getElementById('calculadoraFondo').append(input);
+
+        var input2 = document.createElement("input");
+        input2.type = tipo;
+        input2.name = "displayAnterior";
+        input2.id = "displayAnterior";
+        input2.value = "NE";
+        document.getElementById('calculadoraFondo').append(input2);
+    }
+
+    eventoClic (){
 
         var photo = document.getElementsByClassName( "tecla" );
 
         var ide;
 
         for (var i=0; i < photo.length; i++) {
-            photo[i].onclick = function(){
-
-                for (var i=0; i < photo.length; i++) {
-                    ide = photo[i].id;
-                    document.getElementById(ide).style="cursor:pointer;";
-                };
-
+            photo[i].onmouseover = function(){
                 ide = this.id;
+                document.getElementById(ide).style="cursor:pointer;";
+            }
 
+            photo[i].onmousedown = function(){
+                ide = this.id;
                 Calculadora.adicionarTeclaADisplay(ide);
                 document.getElementById(ide).style="cursor:pointer;width:70px;";
-                console.log();
+            }
+            photo[i].onmouseup = function(){
+                ide = this.id;
+                document.getElementById(ide).style="cursor:pointer;";
             }
         };
     }
@@ -42,23 +63,54 @@ class Calculadora {
 
         var display = document.getElementById("display").textContent;
 
+        var numeros = ["1", "2", "3", "4", "5",  "6",  "7",  "8",  "9",  "0", ];
+        var operadores = ["mas", "menos", "por", "dividido","igual"];
 
-        //alert(display);
+        if(tecla == 'on') {
+            document.getElementById("display").innerHTML = '0';
+            document.getElementById("displayAnterior").value = 'NE';
+            document.getElementById("operSeleccionado").value = '';
+        }else if(numeros.indexOf(tecla) != -1){
+            document.getElementById("display").innerHTML = display=='0' ? tecla : display+tecla;
+        }else if(operadores.indexOf(tecla) != -1){
+
+            var resultado = Calculadora.operar(document.getElementById("operSeleccionado").value, document.getElementById("displayAnterior").value, display)
+
+            document.getElementById("display").innerHTML = tecla == 'igual' ? resultado : '0';
+            document.getElementById("displayAnterior").value = tecla == 'igual' ? 'NE' :resultado;
+            document.getElementById("operSeleccionado").value = tecla == 'igual' ? '' : tecla;
 
 
+        }
 
-        document.getElementById("display").innerHTML = tecla;
-
+        //console.log(tecla);
 
     }
 
+    static operar(oper, valorAnterior, valorActual){
 
-    static distancia ( a , b) {
-        const dx = a.x - b.x;
-        const dy = a.y - b.y;
-
-        return Math.sqrt ( dx * dx + dy * dy );
+        var resultado = 0;
+        switch(oper) {
+            case "mas":
+                resultado = valorAnterior=='NE' ? valorActual : (parseFloat(valorAnterior) + parseFloat(valorActual));
+                break;
+            case "menos":
+                resultado = valorAnterior=='NE' ? valorActual : (parseFloat(valorAnterior) - parseFloat(valorActual));
+                break;
+            case "por":
+                resultado = valorAnterior=='NE' ? valorActual : (parseFloat(valorAnterior) * parseFloat(valorActual));
+                break;
+            case "dividido":
+                resultado = valorAnterior=='NE' ? valorActual : (parseFloat(valorAnterior) / parseFloat(valorActual));
+                break;
+            default:
+                resultado = valorActual;
+        }
+        console.log(oper+'  .... ' +valorAnterior +' '+valorActual+ ' === '+resultado);
+        return resultado;
     }
+
+
 }
 
 
