d7271aad8d24ccc33ba9078387a6d4f018eea65a
Camilo <camilo@e-advising.biz>
2019-03-10 21:00:57 -0500

    Javascript
    *Inicio de clase

--- 5FrameworkLibreriasJavaScript_CamiloArturo_Perez/js/app.js
+++ 5FrameworkLibreriasJavaScript_CamiloArturo_Perez/js/app.js
@@ -4,8 +4,6 @@
 
 $( document ).ready(function() {
 
-
-
     setInterval(function(){
         var el = document.getElementById('tituloJuego');
         if(el.className == 'luzOff'){
@@ -15,6 +13,14 @@ $( document ).ready(function() {
         }
     },500);
 
+
+
+
+    $(".btn-reinicio").click(function(){
+
+        const match = new Match ( {'nombre':'Camilo', 'apellido':'Perez'} );
+        match.reset();
+    });
 
 
 
@@ -43,3 +49,49 @@ $( document ).ready(function() {
 
 
 });
+
+
+
+class Match {
+    constructor ( conf ){
+
+        var usuario = "Diseñado por "+ conf.nombre + ' ' + conf.apellido;
+        console.log(usuario);
+
+        //this.adicionarPropiedades( )  ;
+        //this.eventoClic( )  ;
+
+
+
+    }
+
+    reset(){
+
+        for(var c=1; c<=7; c++) {
+
+            $("."+'col-'+c).html("");
+            for(var f=1; f<=6; f++) {
+                var dulce = Math.floor((Math.random() * 4) + 1);
+                this.adicionarDulce('col-'+c, c+"-"+f, dulce);
+            }
+        }
+
+
+    }
+
+    adicionarDulce(columna, idDulce, dulce){
+        //$("."+columna).append(" <article class=' fila-"+idDulce+"'><img class='elemento' src='image/"+dulce+".png'></article>");
+        $("."+columna).prepend(" <img class='fila-"+idDulce+" elemento' src='image/"+dulce+".png'>");
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
